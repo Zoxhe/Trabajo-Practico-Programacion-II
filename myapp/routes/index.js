8c47f2f6675846588deeecb7117d82c3c895a9e6
ailin fernandez <afernandez@udesa.edu.ar>
2023-04-06 16:57:04 -0300

    nada

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 const productController = require('../controllers/productController')
 
 /* GET home page. */
-// router.get('/', productController.index);
+//router.get('/', productController.index);
 
 module.exports = router;
